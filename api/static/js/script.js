878bd17eaaddd19bbc6e1e39227baa940687ccec
Zenriel <nxll.zen@gmail.com>
2024-06-24 21:10:14 +0700

    V1.3 fixed the animation

--- api/static/js/script.js
+++ api/static/js/script.js
@@ -40,6 +40,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const changeName = "Hello, I'm Fauzan Rizky";
   setTimeout(() => {
     mainName.innerHTML = changeName;
+    P1_D1.style.textShadow = "2px -2px #51EEFC";
     glitchText("P1-D1-H1", changeName);
   }, 2000);
 });
